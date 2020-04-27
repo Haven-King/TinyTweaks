60326931f369be88d5dbcea1990955a6a8234af3
Haven <haven@hephaestus.dev>
2020-04-26 21:51:45 -0700

    Redstone torches can't be placed on grass blocks, for debugging purposes.

---
+++ src/main/java/dev/hephaestus/tweaks/mixin/RedstoneTorchMixin.java
@@ -0,0 +1,19 @@
+package dev.hephaestus.tweaks.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(RedstoneTorchBlock.class)
+public class RedstoneTorchMixin extends TorchBlock {
+    protected RedstoneTorchMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return ((!FabricLoader.getInstance().isDevelopmentEnvironment()) || world.getBlockState(pos.down()).getBlock() != Blocks.GRASS_BLOCK) && super.canPlaceAt(state, world, pos);
+    }
+}
