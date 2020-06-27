081e64a437ae5cb78b7fc84219b20b7ccc781044
Haven <haven@hephaestus.dev>
2020-06-27 12:37:41 -0700

    Fixes Issue #7

---
+++ src/main/java/dev/hephaestus/tweaks/mixin/entity/InteractableEntityMixin.java
@@ -0,0 +1,24 @@
+package dev.hephaestus.tweaks.mixin.entity;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(targets = {
+		"net/minecraft/entity/passive/VillagerEntity",
+		"net/minecraft/entity/passive/WanderingTraderEntity",
+		"net/minecraft/entity/mob/PiglinEntity"
+})
+public class InteractableEntityMixin {
+	@Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
+	private void useFlintAndSteel(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+		if (player.getStackInHand(hand).getItem().equals(Items.FLINT_AND_STEEL)) {
+			cir.setReturnValue(ActionResult.PASS);
+		}
+	}
+}
