61c73ccb34c27d43ac19117694f9861465006dbf
Haven <haven@hephaestus.dev>
2020-06-27 11:28:28 -0700

    Somehow my gradle files didn't actually push? Whatever. Here we go again!

--- src/main/java/dev/hephaestus/tweaks/mixin/client/gui/screen/ingame/SignEditScreenMixin.java
+++ src/main/java/dev/hephaestus/tweaks/mixin/client/gui/screen/ingame/SignEditScreenMixin.java
@@ -6,16 +6,12 @@ import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.client.gui.screen.ingame.SignEditScreen;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.StringRenderable;
-import net.minecraft.text.Style;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.List;
 
 @Mixin(SignEditScreen.class)
 @Environment(EnvType.CLIENT)
