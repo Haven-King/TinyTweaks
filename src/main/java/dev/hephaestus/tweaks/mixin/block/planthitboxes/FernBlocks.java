ec2983d25fe90a7e874c8e63da7d33c707f07135
Haven <haven@hephaestus.dev>
2021-02-08 13:15:11 -0800

    Fixes issue with fern block hitboxes

--- src/main/java/dev/hephaestus/tweaks/mixin/block/planthitboxes/FernBlocks.java
+++ src/main/java/dev/hephaestus/tweaks/mixin/block/planthitboxes/FernBlocks.java
@@ -2,19 +2,18 @@ package dev.hephaestus.tweaks.mixin.block.planthitboxes;
 
 import dev.hephaestus.tweaks.Tweaks;
 import dev.hephaestus.tweaks.util.ItemEntityShapeContext;
-import net.minecraft.block.*;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FernBlock;
+import net.minecraft.block.PlantBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(FernBlock.class)
 public class FernBlocks extends PlantBlock {
-    @Shadow @Final protected static VoxelShape SHAPE;
-
     protected FernBlocks(Settings settings) {
         super(settings);
     }
@@ -24,7 +23,7 @@ public class FernBlocks extends PlantBlock {
         if (!Tweaks.CONFIG.plantHitboxes && !(context instanceof ItemEntityShapeContext && ((ItemEntityShapeContext) context).getItem().isIn(Tweaks.SHOWS_GRASS_HITBOXES))) {
             return VoxelShapes.empty();
         } else {
-            return SHAPE;
+            return super.getOutlineShape(state, view, pos, context);
         }
     }
 }
